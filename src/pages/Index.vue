2362739bf7cf02c797e954985b9cbe418dc84cea
mirosnicenkoo19 <miroshnichenkoo19@gmail.com>
2019-04-12 09:21:37 +0300

    to git

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="justify-center">
+    <div class="col-12 col-md-12 justify-center">Привет, ITConnect 2019</div>
+    <div class="col-12 col-md-12 justify-center">
+      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    </div>
+  </q-page>
+</template>
+
+<style>
+  .text {
+    color: blue;
+  }
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
