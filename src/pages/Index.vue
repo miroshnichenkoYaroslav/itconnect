a0841752f3016f5a0fd3e4234c925c8c3ee0c26b
mirosnicenkoo19 <miroshnichenkoo19@gmail.com>
2019-04-12 09:36:44 +0300

    Добавил стили на главный экран

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-page class="justify-center">
-    <div class="col-12 col-md-12 justify-center">Привет, ITConnect 2019</div>
-    <div class="col-12 col-md-12 justify-center">
+  <q-page class="justify-center column align-items-center">
+    <div class="text-center text">Привет, ITConnect 2019</div>
+    <div class="text-center">
       <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
     </div>
   </q-page>
@@ -9,7 +9,9 @@
 
 <style>
   .text {
+    margin-bottom: 30px;
     color: blue;
+    font-size: 30px;
   }
 </style>
 
